090c6e22315cb6392fb02ae76fffa64d1b05819d
Wesley <wesley_snaine@hotmail.com>
2022-01-12 11:06:19 -0300

    create the logic to display the board on the page and player be able to click and give mark

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+
+
 const gameContainer = document.querySelector('.gameContainer');
 
 const Gameboard = (() => {
-    let gameboard = ['X','O','','','X','','O','x','O'];
+    let gameboard = ['X','O','','','X','','O','X','O'];
     const updateBoard = () => {
+        while (gameContainer.firstChild) {
+            gameContainer.removeChild(gameContainer.firstChild);
+        }
         for (let i = 0; i < 9; i++) {
             const newSquare = document.createElement('div');
             newSquare.textContent = gameboard[i];
             newSquare.classList.add('square');
+            newSquare.addEventListener('click', function (e) {
+                currentPlayer.play(Array.from(document.querySelectorAll('.gameContainer > .square')).indexOf(e.target));
+            });
             gameContainer.appendChild(newSquare);
         };
     };
@@ -16,6 +24,11 @@ const Gameboard = (() => {
 const PlayerGenerator = (name, mark) => {
     const play = (index) => {
         Gameboard.gameboard[index] = mark;
+        Gameboard.updateBoard();
     };
     return {name, mark, play};
 };
+
+const player1 = PlayerGenerator('Jack', 'X');
+const player2 = PlayerGenerator('Jame', 'O');
+let currentPlayer = player1;
