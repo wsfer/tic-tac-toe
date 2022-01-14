caa921e5af63a7fb587fbdf050d449db5597da90
Wesley <wesley_snaine@hotmail.com>
2022-01-14 10:36:45 -0300

    create a function to check for a winner and change background color from winner's squares

--- script.js
+++ script.js
@@ -10,8 +10,16 @@ const PlayerGenerator = (name, mark) => {
     const play = (index) => {
         Gameboard.gameboard[index] = mark;
         gameContainer.children[index].textContent = mark;
+        Gameboard.checkWinner(0, 1, 2);
+        Gameboard.checkWinner(3, 4, 5);
+        Gameboard.checkWinner(6, 7, 8);
+        Gameboard.checkWinner(0, 3, 6);
+        Gameboard.checkWinner(1, 4, 7);
+        Gameboard.checkWinner(2, 5, 8);
+        Gameboard.checkWinner(0, 4, 8);
+        Gameboard.checkWinner(2, 4, 6);
     };
-    return {name, play, round};
+    return {name, mark, play, round};
 };
 
 const gameController = (() => {
@@ -42,6 +50,7 @@ const gameController = (() => {
         clearBoard();
         createMenu();
         scoreBoard.textContent = `Game starting ...`;
+        Gameboard.gameboard = ['','','','','','','','',''];
     };
     const createInputs = () => {
         element = document.createElement('label');
@@ -59,6 +68,7 @@ const gameController = (() => {
         element.setAttribute('id', 'playerTwo')
         gameContainer.appendChild(element);
     };
+    //This will create the interface to get player names.
     const getNames = () => {
         clearBoard();
         let element = document.createElement('h3');
@@ -84,12 +94,13 @@ const gameController = (() => {
 })();
 
 const Gameboard = (() => {
-    let gameboard = ['X','O','','','X','','O','X','O'];
+    let gameboard = ['','','','','','','','',''];
     const create = () => {
         const event = function (e) {
             whoIsNext().play(Array.from(document.querySelectorAll('.gameContainer > .square')).indexOf(e.target));
             e.target.removeEventListener('click', event);
         };
+        gameContainer.style.flexDirection = "row";
         for (let i = 0; i < 9; i++) {
             const newSquare = document.createElement('div');
             newSquare.classList.add('square');
@@ -98,6 +109,12 @@ const Gameboard = (() => {
         };
     };
     const whoIsNext = () => {
+        if (player1.round == player2.round) {
+            player1.round = Math.random();
+            player2.round = Math.random();
+            whoIsNext();
+            //We never know when Math.random will troll...
+        };
         if (player1.round > player2.round) {
             player1.round = 0;
             player2.round = 1;
@@ -106,15 +123,26 @@ const Gameboard = (() => {
             player1.round = 1;
             player2.round = 0;
             return player2;
-        }
-    }
-    const update = () => {
-        for (let i = 0; i < 9; i++) {
-            const newSquare = document.createElement('div');
-            newSquare.textContent = gameboard[i];
         };
     };
-    return {gameboard, create, update};
+    const checkWinner = (a, b, c) => {
+        if (Gameboard.gameboard[a] === 'X') {
+            if (Gameboard.gameboard[a] === Gameboard.gameboard[b] && Gameboard.gameboard[b] === Gameboard.gameboard[c]) {
+                gameContainer.children[a].style.backgroundColor = "orange";
+                gameContainer.children[b].style.backgroundColor = "orange";
+                gameContainer.children[c].style.backgroundColor = "orange";
+                console.log(`${player2.name} is the Winner!`);
+            };
+        } else if (Gameboard.gameboard[a] === 'O') {
+            if (Gameboard.gameboard[a] === Gameboard.gameboard[b] && Gameboard.gameboard[b] === Gameboard.gameboard[c]) {
+                gameContainer.children[a].style.backgroundColor = "orange";
+                gameContainer.children[b].style.backgroundColor = "orange";
+                gameContainer.children[c].style.backgroundColor = "orange";
+                console.log(`${player1.name} is the Winner!`);
+            };
+        };
+    };
+    return {gameboard, create, checkWinner};
 })();
 
 restartBtn.addEventListener('click', gameController.newGame);
