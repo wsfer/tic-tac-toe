abe4e8db04252aac464a7a2207d43eb684169b28
Wesley <wesley_snaine@hotmail.com>
2022-01-18 23:40:23 -0300

    update functions for a future AI

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const newRndBtn = document.querySelector('.newRound');
 
 let player1;
 let player2;
+let nextPlayer = [];
 
 const PlayerGenerator = (name, mark) => {
-    let round = Math.random();
     let _score = 0;
+    let isComputer = false;
+    const getMark = () => mark;
     const getScore = () => _score;
     const getName = () => {
         if (!name) {
@@ -20,14 +22,15 @@ const PlayerGenerator = (name, mark) => {
         };
     };
     const play = (index) => {
+        nextPlayer.push(nextPlayer[0]);
         let index2 = 0;
         gameContainer.children[index].textContent = mark;
         while (index > 2) {
             index2++;
             index = index - 3;
         }
-        Gameboard.gameboard[index][index2] = mark;
-        if (Gameboard.checkWinner(mark)) {
+        gameController.gameboard[index][index2] = mark;
+        if (gameController.checkWinner(mark)) {
             _score++;
             if (_score === 3) {
                 scoreBoard.textContent = `${getName()} is the Winner`;
@@ -36,42 +39,46 @@ const PlayerGenerator = (name, mark) => {
                 scoreBoard.textContent = `${getName()} won the round`;
             };
         };
+        nextPlayer.shift();
+        if (nextPlayer[0].isComputer) {
+            setTimeout(() => {nextPlayer[0].play(Math.round(Math.random()*8))}, 1000);
+        };
     };
-    return {getName, getScore, play, round};
+    return {getName, getScore, play, getMark, isComputer};
 };
 
 const displayController = (() => {
-    const clearBoard = () => {
+    const _clearBoard = () => {
         while (gameContainer.firstChild) {
             gameContainer.removeChild(gameContainer.firstChild);
         };
     };
     //This will create initial screen.
-    const createMenu = () => {
+    const _createMenu = () => {
         let element = document.createElement('h3');
         element.textContent = "How do you like to play?";
         gameContainer.appendChild(element);
         element = document.createElement('button');
         element.textContent = "Player vs Player";
-        element.addEventListener('click', getNamesMenu);
+        element.addEventListener('click', _getNamesMenu);
         gameContainer.appendChild(element);
         element = document.createElement('button');
         element.textContent = "Player vs Computer (working)";
-        element.addEventListener('click', getPlayerNameMenu);
+        element.addEventListener('click', _getPlayerNameMenu);
         gameContainer.appendChild(element);
         gameContainer.style.flexDirection = "column";
     };
     //This will clear everything and create initial screen.
     const newGame = () => {
-        clearBoard();
-        createMenu();
+        _clearBoard();
+        _createMenu();
         newRndBtn.style.visibility = "hidden";
         scoreBoard.textContent = '---';
-        Gameboard.gameboard = [['','',''],['','',''],['','','']];
+        gameController.gameboard = [['','',''],['','',''],['','','']];
     };
     //This will be called when multiplayer is selected.
-    const getNamesMenu = () => {
-        clearBoard();
+    const _getNamesMenu = () => {
+        _clearBoard();
         let element = document.createElement('h3');
         element.textContent = "Player Names: ";
         gameContainer.appendChild(element);
@@ -91,12 +98,12 @@ const displayController = (() => {
         gameContainer.appendChild(element);
         element = document.createElement('button');
         element.textContent = "Start Game";
-        element.addEventListener('click', startGame);
+        element.addEventListener('click', _startGame);
         gameContainer.appendChild(element);
     };
     //This will be called when play agaisnt computer is selected.
-    const getPlayerNameMenu = () => {
-        clearBoard();
+    const _getPlayerNameMenu = () => {
+        _clearBoard();
         let element = document.createElement('h3');
         element.textContent = "Your name: ";
         gameContainer.appendChild(element);
@@ -105,46 +112,41 @@ const displayController = (() => {
         gameContainer.appendChild(element);
         element = document.createElement('button');
         element.textContent = "Start Game";
-        element.addEventListener('click', startGame);
+        element.addEventListener('click', _startGame);
         gameContainer.appendChild(element)
 
     }
-    const startGame = () => {
+    const _startGame = () => {
         player1 = PlayerGenerator(document.getElementById('playerOne').value, 'O');
         if (document.getElementById('playerTwo')) {
             player2 = PlayerGenerator(document.getElementById('playerTwo').value, 'X');
             computerPlay = false;
         } else {
-            player2 = PlayerGenerator(randomName(), 'X');
+            player2 = PlayerGenerator(_randomName(), 'X');
             computerPlay = true;
-        }
-        
-        clearBoard();
-        updateScoreBoard();
-        Gameboard.create();
+            player2.isComputer = true;
+        };
+        const whoWillStart = (() => {
+            let p1 = Math.random();
+            let p2 = Math.random();
+            if (p1 > p2) {
+                nextPlayer.push(player1);
+                nextPlayer.push(player2);
+            } else {
+                nextPlayer.push(player2);
+                nextPlayer.push(player1);
+            }
+        })();
+        _clearBoard();
+        _updateScoreBoard();
+        _createGameboard();
+        if (nextPlayer[0].isComputer) {
+            nextPlayer[0].play(Math.round(Math.random()*8));
+        };
     };
-    const randomName = () => {
-        let names = ['Mark', 'Beth', 'Miguel', 'Marcos', 'Sasha'];
-        return names[Math.round(Math.random()*names.length)];
-    }
-    const newRound = () => {
-        displayController.clearBoard();
-        Gameboard.create();
-        displayController.updateScoreBoard();
-        newRndBtn.style.visibility = "hidden";
-        Gameboard.gameboard = [['','',''],['','',''],['','','']];
-    }
-    const updateScoreBoard = () => {
-        scoreBoard.textContent = `${player1.getName()} ${player1.getScore()} - ${player2.getScore()} ${player2.getName()}`;
-    };
-    return {newGame, updateScoreBoard, newRound, clearBoard};
-})();
-
-const Gameboard = (() => {
-    let gameboard = [['','',''],['','',''],['','','']];
-    const create = () => {
+    const _createGameboard = () => {
         const event = function (e) {
-            whoIsNext().play(Array.from(document.querySelectorAll('.gameContainer > .square')).indexOf(e.target));
+            nextPlayer[0].play(Array.from(document.querySelectorAll('.gameContainer > .square')).indexOf(e.target));
             e.target.removeEventListener('click', event);
         };
         gameContainer.style.flexDirection = "row";
@@ -155,51 +157,53 @@ const Gameboard = (() => {
             gameContainer.appendChild(newSquare);
         };
     };
-    const whoIsNext = () => {
-        if (player1.round == player2.round) {
-            player1.round = Math.random();
-            player2.round = Math.random();
-            whoIsNext();
-            //We never know when Math.random will troll...
-        };
-        if (player1.round > player2.round) {
-            player1.round = 0;
-            player2.round = 1;
-            return player1;
-        } else {
-            player1.round = 1;
-            player2.round = 0;
-            return player2;
-        };
+    const _randomName = () => {
+        let names = ['Mark', 'Beth', 'Miguel', 'Marcos', 'Sasha'];
+        return names[Math.round(Math.random()*names.length)];
+    }
+    const newRound = () => {
+        _clearBoard();
+        _createGameboard();
+        _updateScoreBoard();
+        newRndBtn.style.visibility = "hidden";
+        gameController.gameboard = [['','',''],['','',''],['','','']];
+    }
+    const _updateScoreBoard = () => {
+        scoreBoard.textContent = `${player1.getName()} ${player1.getScore()} - ${player2.getScore()} ${player2.getName()}`;
     };
+    return {newGame, newRound};
+})();
+
+const gameController = (() => {
+    let gameboard = [['','',''],['','',''],['','','']];
     const checkWinner = (mark) => {
         //Check rows.
         for (let i = 0; i < 3; i++) {
-            if (Gameboard.gameboard[0][i] === mark &&
-                Gameboard.gameboard[1][i] === mark &&
-                Gameboard.gameboard[2][i] === mark) {
+            if (gameController.gameboard[0][i] === mark &&
+                gameController.gameboard[1][i] === mark &&
+                gameController.gameboard[2][i] === mark) {
                 _changeSquaresColor(i*3, i*3+1, i*3+2);
                 return true;
             };
         };
         //Check columns.
         for (let i = 0; i < 3; i++) {
-            if (Gameboard.gameboard[i][0] === mark &&
-                Gameboard.gameboard[i][1] === mark &&
-                Gameboard.gameboard[i][2] === mark) {
+            if (gameController.gameboard[i][0] === mark &&
+                gameController.gameboard[i][1] === mark &&
+                gameController.gameboard[i][2] === mark) {
                 _changeSquaresColor(i, i+3, i+6);
                 return true;
             };
         };
         //Check diagonals.
-        if (Gameboard.gameboard[0][0] === mark &&
-            Gameboard.gameboard[1][1] === mark &&
-            Gameboard.gameboard[2][2] === mark) {
+        if (gameController.gameboard[0][0] === mark &&
+            gameController.gameboard[1][1] === mark &&
+            gameController.gameboard[2][2] === mark) {
             _changeSquaresColor(0, 4, 8);
             return true;
-        } else if (Gameboard.gameboard[2][0] === mark &&
-            Gameboard.gameboard[1][1] === mark &&
-            Gameboard.gameboard[0][2] === mark) {
+        } else if (gameController.gameboard[2][0] === mark &&
+            gameController.gameboard[1][1] === mark &&
+            gameController.gameboard[0][2] === mark) {
             _changeSquaresColor(2, 4, 6);
             return true;
         };
@@ -208,8 +212,8 @@ const Gameboard = (() => {
         gameContainer.children[index1].style.backgroundColor = "orange";
         gameContainer.children[index2].style.backgroundColor = "orange";
         gameContainer.children[index3].style.backgroundColor = "orange";
-    }
-    return {gameboard, create, checkWinner, whoIsNext};
+    };
+    return {gameboard, checkWinner};
 })();
 
 restartBtn.addEventListener('click', displayController.newGame);
@@ -220,13 +224,12 @@ displayController.newGame();
 //Computer AI
 
 const computerAI = (() => {
-    const easyAI = () => {
-        let randomNumber = Math.round(Math.random()*9);
-        if (Gameboard.gameboard[randomNumber] === '') {
-            Gameboard.whoIsNext().play(randomNumber);
-        } else {
-            easyAI();
-        };
+    let _enemy = player1;
+    const willEnemyWin = () => {
+
     };
-    return {easyAI};
+    const computerMove = () => {
+
+    };
+    return {computerMove};
 })();
